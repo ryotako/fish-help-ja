320c8ddc58435400c85dea64918feeeedeaf7e7d
ryotako <kobayashiryota2046@gmail.com>
2017-01-22 13:34:15 +0900

    fix an argument cout check

--- functions/help-ja.fish
+++ functions/help-ja.fish
@@ -1,18 +1,13 @@
 function help-ja --description 'Show Japanese help for the fish shell'
-	set -l topics (builtin -n) tutorial document-ja faq-ja design-document-ja
-  
+  set -l topics (builtin -n) tutorial document-ja faq-ja design-document-ja
   set -l home 'http://fish.rubikitch.com'
 
-  if count $argv >/dev/null
-    switch $argv[1]
-      case -h --h --he --hel --help
-        echo 'Usage: help-ja [TOPIC]'
-      case $topics
-        open "$home/$argv[1]"
-      case '*'
-        open $home
-    end
-  else
-    open $home
+  switch "$argv[1]"
+    case -h --h --he --hel --help
+      echo 'Usage: help-ja [TOPIC]'
+    case $topics
+      open "$home/$argv[1]"
+    case '*'
+      open $home
   end
 end
